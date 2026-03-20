f6b22d25a8a8b93d07bcab10998874004fe3d964
xueyong <xueyong01@zuoyebang.com>
2026-03-20 15:28:49 +0800

    refactor: remove old ImageRecognitionService (merged into LLMExtractionService)

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,10 +1,8 @@
 """服务模块"""
 
-from .image_recognition_service import ImageRecognitionService
 from .llm_extraction_service import LLMExtractionService, ExtractedInfo
 
 __all__ = [
-    'ImageRecognitionService',
     'LLMExtractionService',
     'ExtractedInfo',
 ]
